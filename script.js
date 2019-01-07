5c2d166eea3347a21767be2436e85eea0964c35b
liudijun <362172145@qq.com>
2019-01-07 22:11:32 +0800

    0.0.1
    优化UI

--- script.js
+++ script.js
@@ -12,15 +12,14 @@ let addBut = document.getElementsByClassName("addItemBut")[0],
     newDomHtml = ' <div class="itemContent addItemCont  bg-gray">\n' +
         '                <input class="messageContent">\n' +
         '                <div class="startContent">\n' +
-        '                    <span class="bg-red"></span>\n' +
-        '                    <span class="bg-orange"></span>\n' +
-        '                    <span class="bg-yellow"></span>\n' +
-        '                    <span class="bg-green"></span>\n' +
+        '                    <span title="优先级为一" name="red" class="bg-red icon-faster">&#xe641;</span>\n' +
+        '                    <span title="优先级为二" name="orange" class="bg-orange icon-fast">&#xe649;</span>\n' +
+        '                    <span title="优先级为三" name="green" class="bg-green icon-normal">&#xe618;</span>\n' +
         '                </div>\n' +
         '            </div>\n' +
         '            <div class="newMenu">\n' +
-        '                <span class="isOK bg-green">&#xe7fc;</span>\n' +
-        '                <span class="isNo bg-red">&#xe7fd;</span>\n' +
+        '                <span class="icon-isOK bg-green">&#xe502;</span>\n' +
+        '                <span class="icon-isNo bg-red">&#xe654;</span>\n' +
         '            </div>',
     content = document.getElementsByClassName('content')[0];
 
@@ -39,13 +38,14 @@ addBut.addEventListener("click",function () {
         newDom.className = "newItem";
         newDom.innerHTML = newDomHtml;
         content.insertBefore(newDom,itemFirstC);
-        let isOkBut = document.getElementsByClassName('isOK')[0],
+        let isOkBut = document.getElementsByClassName('icon-isOK')[0],
             colorBut = document.getElementsByClassName('startContent')[0],
             colorButArray = toArray(colorBut.children);
         //确认添加按钮
         isOkBut.addEventListener("click",function () {
             newDom.children[0].children[1].remove();
             newDom.children[1].remove();
+            newDom.children[0].children[0].style.width = "100%";
             content.children[0].children[0].style.width = "100%";
             addICBg.style.display = 'none';
         })
